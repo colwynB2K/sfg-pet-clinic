3ef50fe4f60ea63e2a519f348f545e6646adb407
Ivo Eersels <ivo.eersels@gmail.com>
2020-08-23 12:18:57 +0200

    Add id to model objects, by adding in a BaseEntity. Closes #9.

---
+++ pet-clinic-data/src/main/java/guru/springframework/model/BaseEntity.java
@@ -0,0 +1,16 @@
+package guru.springframework.model;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+
+    private Long id; // Hibernate recommends to use 'boxes types' (like Integer, Long, etc...) instead of primitives (int, long,... ) for ids as these can be null, while primitives cannot
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
