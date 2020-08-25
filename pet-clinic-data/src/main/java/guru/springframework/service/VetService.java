782abb7a7577a1aa5d02dc522325e29b034ad17b
Ivo Eersels <ivo.eersels@idewe.be>
2020-08-25 09:15:23 +0200

    Refactor Services to common base CrudService Interface. Closes #10.
    By mimicing the CrudRepository approach from Spring Data we can clean up all the duplicated code in the Services, because all have similar
    findById(), save() and findAll() methods.
    
    So we will add a new generic CrudService interface which uses generics and mimics the API of CrudRepository from Spring Data
    
    public interface CrudService<T, ID> {
    
        Set<T> findAll();
    
        T findById(ID id);
    
        T save(T object);
    
        void delete(T object);
    
        void deleteById(ID id);
    }
    
    By extending from the interface in the existing services, we can now remove the 'duplicated' methods ans have a much cleaner codebase.
    
    public interface OwnerService extends CrudService<Owner, Long> {
    
        Owner findByLastName(String lastName);
    
    }
    
    public interface PetService extends CrudService<Pet, Long> {
    }
    
    public interface VetService extends CrudService<Vet, Long> {
    }

--- pet-clinic-data/src/main/java/guru/springframework/service/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/service/VetService.java
@@ -2,13 +2,5 @@ package guru.springframework.service;
 
 import guru.springframework.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet pet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
