275a0188ee18d48c8d6ab12ecc040490dda86809
eersels3764 <ivo.eersels@idewe.be>
2020-08-31 17:51:22 +0200

    Create Specialties, add to Vets on Startup (Bootstrap). Closes #33
    Forgot to properly implement the saving of a Vet object in VetMapService

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/VetMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/map/VetMapService.java
@@ -1,13 +1,24 @@
 package guru.springframework.sfgpetclinic.service.map;
 
+import guru.springframework.sfgpetclinic.model.Specialty;
 import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.service.SpecialtyService;
 import guru.springframework.sfgpetclinic.service.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialtyService specialtyService;
+
+    @Autowired
+    public VetMapService(SpecialtyService specialtyService) {
+        this.specialtyService = specialtyService;
+    }
+
     @Override
     public Set<Vet> findAll() {
         return super.findAll();         // Call the method of the AbstractMapService with the correct Type
@@ -25,7 +36,19 @@ public class VetMapService extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet vet) {
-        return super.save(vet);
+        if (vet != null) {
+            Set<Specialty> specialties = vet.getSpecialties();
+            specialties.forEach(specialty -> {
+                if (specialty.getId() == null) {                                            // If the current Specialty wasn't persisted yet
+                    Specialty savedSpecialtyWithId = specialtyService.save(specialty);      // save it
+                    specialty.setId(savedSpecialtyWithId.getId());                          // Make sure to stored the saved specialty id on the current Specialty object
+                }
+            });
+
+            return super.save(vet);
+        } else {
+            throw new RuntimeException("vet object can't be null!");
+        }
     }
 
     @Override
