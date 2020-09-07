c9f0c5b1e59e0c8232731c4507f998e911967c23
eersels3764 <ivo.eersels@idewe.be>
2020-09-07 17:07:34 +0200

    Create Visit Entity. Closes #37

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package guru.springframework.sfgpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pet")
@@ -20,6 +22,9 @@ public class Pet extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")                         // When we delete the pet, also delete the associated visits
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -53,4 +58,11 @@ public class Pet extends BaseEntity {
         this.owner = owner;
     }
 
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
+    }
 }
