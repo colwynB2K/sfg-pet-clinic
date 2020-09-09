ab3e61eb8638ba751b045842068c25ffbae8114a
eersels3764 <ivo.eersels@idewe.be>
2020-09-09 14:43:02 +0200

    Create Spring Data JPA Service for Pet Type. Closes #41

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/springdatajpa/PetTypeRepositoryService.java
@@ -0,0 +1,52 @@
+package guru.springframework.sfgpetclinic.service.springdatajpa;
+
+import guru.springframework.sfgpetclinic.exception.ObjectNotFoundException;
+import guru.springframework.sfgpetclinic.model.PetType;
+import guru.springframework.sfgpetclinic.repository.PetTypeRepository;
+import guru.springframework.sfgpetclinic.service.PetTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("SpringDataJPA")
+public class PetTypeRepositoryService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    @Autowired
+    public PetTypeRepositoryService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return petTypeRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("No PetType found for id '"+ id + "'"));
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return petTypeRepository.save(petType);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        petTypeRepository.delete(petType);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petTypeRepository.deleteById(id);
+    }
+}
