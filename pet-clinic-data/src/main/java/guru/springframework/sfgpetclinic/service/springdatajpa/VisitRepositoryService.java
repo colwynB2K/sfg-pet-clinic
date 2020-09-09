8a4587cbe1a374e2dd87f7266413f50feb35b31a
eersels3764 <ivo.eersels@idewe.be>
2020-09-09 16:08:04 +0200

    Create Visit Data JPA Service. Closes #46

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/springdatajpa/VisitRepositoryService.java
@@ -0,0 +1,52 @@
+package guru.springframework.sfgpetclinic.service.springdatajpa;
+
+import guru.springframework.sfgpetclinic.exception.ObjectNotFoundException;
+import guru.springframework.sfgpetclinic.model.Visit;
+import guru.springframework.sfgpetclinic.repository.VisitRepository;
+import guru.springframework.sfgpetclinic.service.VisitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("SpringDataJPA")
+public class VisitRepositoryService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    @Autowired
+    public VisitRepositoryService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return visitRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException(""));
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        return visitRepository.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        visitRepository.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        visitRepository.deleteById(id);
+    }
+}
